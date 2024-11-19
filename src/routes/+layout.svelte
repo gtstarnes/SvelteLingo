49c9d470190ad6f394e292441b66600bc087d2d1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:15:20 -0500

    added footer to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Footer from "../components/Footer.svelte";
+import Header from "../components/Header.svelte";
+    let { children } = $props();
+
+</script>
+
+<main>
+    <Header />
+    {@render children()}
+    <Footer />
+</main>
