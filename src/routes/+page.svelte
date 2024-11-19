8333138b05e35ddf7f53d90dea5eb95d1cfc8827
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:24:37 -0500

    added basic styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,20 @@
 </script>
 
 <div>
-    <div>
+    <div class="buttonContainer">
         <MainButton />
-        <div>Or</div>
+        <div class="or">Or</div>
         <MainButton />
     </div>
 </div>
 
 <style>
-
+    .buttonContainer {
+        display: flex;
+        gap: 4rem;
+    }
+    .or {
+        border-top: 2px solid black;
+        border-bottom: 2px solid black;
+    }
 </style>
