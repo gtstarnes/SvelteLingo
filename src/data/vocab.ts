84bad62c7b2bb07701cad3d019236b4db72f833c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:39:12 -0500

    created basic vocab entry

---
+++ src/data/vocab.ts
@@ -0,0 +1,12 @@
+
+
+const vocab = {
+    one: {
+        en: {
+            hello: {ch: "nihao", es: "hola", de:'hallo', gr:"yia sou", lat:'salve', sw: "hej"}
+        }
+    }
+}
+
+
+export default vocab
