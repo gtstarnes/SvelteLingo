a6945213f6d5cc2899cb4b4324383000f647d5f8
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:07:14 -0500

    created basic nav bar

---
+++ src/components/navBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<nav>
+    <div>TITLE</div>
+    <div>LOG IN</div>
+</nav>
+
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
