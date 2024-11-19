b8999d70aaea239e4c7d51972a02bba3160f3b90
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:22:40 -0500

    created basic homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import MainButton from "../components/MainButton.svelte";
+
+
+</script>
+
+<div>
+    <div>
+        <MainButton />
+        <div>Or</div>
+        <MainButton />
+    </div>
+</div>
+
+<style>
+
+</style>
