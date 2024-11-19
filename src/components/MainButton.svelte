081145b0dbb21ee83c0462a3ba1cfd2847d477c4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:22:29 -0500

    created button component

---
+++ src/components/MainButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+export let text = 'button'
+export let color = 'lightgray'
+</script>
+
+
+<button>
+    <div style={`background-color: ${color}`}>{text}</div>
+</button>
+
+
+<style>
+ button {
+    height: 1rem;
+    width: 2rem;
+    border-radius: 2%;
+ }
+</style>
