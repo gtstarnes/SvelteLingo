8103fd9a48f56ff24cb10e2055b8c23c81f8b3a7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 20:42:54 -0500

    created types for vocab

--- src/data/vocab.ts
+++ src/data/vocab.ts
@@ -1,6 +1,20 @@
+interface Vocab {
+    [unit: string]: Language;
+}
 
+interface Language{
+    [languageCode: string]: WordEntry;
+}
 
-const vocab = {
+interface WordEntry {
+    [word: string]: Translation;
+}
+
+interface Translation {
+    [languageCode:string]: string;
+}
+
+const vocab: Vocab = {
     one: {
         en: {
             hello: {ch: "nihao", es: "hola", de:'hallo', gr:"yia sou", lat:'salve', sw: "hej"}
